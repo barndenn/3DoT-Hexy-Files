ac5d94ad32eb2c7efd4d6428eeb60411b0aac054
barndenn <brandonnguyen1997@gmail.com>
2018-12-03 21:14:01 -0800

    Add files via upload

---
+++ 3DotConfig.ino
@@ -0,0 +1,20 @@
+void Init3DoT() {     
+  // Motor Driver = OUTPUT
+  pinMode(AIN1,OUTPUT);
+  pinMode(AIN2,OUTPUT);
+  pinMode(PWMA,OUTPUT);
+  pinMode(STBY,OUTPUT);
+  pinMode(BIN1,OUTPUT);
+  pinMode(BIN2,OUTPUT);
+  pinMode(PWMB,OUTPUT);
+
+  // Ulrasonic Sensors
+//  pinMode(trigPin, OUTPUT);
+//  pinMode(echoPin, INPUT);
+
+  //Absolute phase encoder
+  pinMode(phaR, INPUT_PULLUP);
+  pinMode(phaL, INPUT_PULLUP);
+  
+  Serial.begin(9600);
+}
